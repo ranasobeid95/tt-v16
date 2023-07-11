e9accf54f6241fb450d0ff466569def7bf958f03
ranasobeid95 <ranasobeid95@gmail.com>
2023-07-11 23:06:46 +0300

    add style and images

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less']
+})
+export class HomeComponent  implements OnInit{
+
+  @Input() hideHeader!:boolean;
+
+  ngOnInit() {
+    console.log('data :>> ', this.hideHeader);
+  }
+
+  GoToSignIn(){
+    
+  }
+
+
+}
